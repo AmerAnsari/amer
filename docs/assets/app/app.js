add04b800f765a0e44e9baac909ca02685282477
Amer Ansari <amer@savandbros.com>
2020-05-19 03:37:58 +0400

    Another way fot http client!

--- docs/assets/app/app.js
+++ docs/assets/app/app.js
@@ -13,9 +13,18 @@ $(document).on('ready scroll', function () {
  * Update current year in footer
  */
 $(document).ready(function () {
-  /**
-   * Api call
-   */
+  console.info('Nothing here except your legend...');
+  $('#year').text(new Date().getFullYear());
+});
+
+/**
+ * Do something on page load
+ */
+window.addEventListener("load", apicall());
+/**
+ * Api call
+ */
+function apicall() {
   const http = new XMLHttpRequest();
   const url = 'https://amer-js.herokuapp.com/hello';
   http.open('POST', url, true);
@@ -23,14 +32,13 @@ $(document).ready(function () {
 //Send the proper header information along with the request
   http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-  http.onreadystatechange = function() {//Call a function when the state changes.
+  http.onreadystatechange = function() {    //Call a function when the state changes.
     if(http.readyState === 4 && http.status === 200) {
       alert(http.responseText);
+      console.log("Api was called!");
+    } else {
+      console.log("Gah, api is not working!!");
     }
   }
   http.send();
-
-
-  console.info('Nothing here except your legend...');
-  $('#year').text(new Date().getFullYear());
-});
+}
